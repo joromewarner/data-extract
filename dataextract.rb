4510bf420dc0b2d071f11761b8e19f0e02ead74d
Jorome Warner <warnerjorome@gmail.com>
2017-10-23 20:54:27 -0400

    Modified Code

--- dataextract.rb
+++ dataextract.rb
@@ -6,27 +6,48 @@ require 'gmail'
 require 'nokogiri' 
 require 'selenium-webdriver'
 
+# Pass File Into Script
+
+filename = "urls.txt"
+
+list = File.new(filename, "r")
+
+arr = list.readlines
+
+list.close
+
+
 # Load Website To Test
 
-urls = "https://www.webpagetest.org"
 driver = Selenium::WebDriver.for :firefox
-driver.navigate.to "#{urls}"
+
+emailbody = ""
+
+#Looping The Website
+#for urls in arr can also be used
+arr.each do |urls|
+
+driver.navigate.to "https://www.webpagetest.org"
+
 
 # Input to Test Website
 
-url = "https://www.Joshuakemp.blogspot.com"
 websiteurl = driver.find_element(:id, "url")
+
 websiteurl.clear()
-websiteurl.send_keys "#{url}"
+
+websiteurl.send_keys urls
 
 # Click Button
 
 SubmitButton = driver.find_element(:class, "start_test")
+
 SubmitButton.click
 
 # Timeout/Idle code then check for elements
 
 wait = Selenium::WebDriver::Wait.new(:timeout => 300)
+
 loadtime = wait.until {
     element = driver.find_element(:id, "LoadTime")
     element if element.displayed?
@@ -37,19 +58,26 @@ element = driver.find_element(:id, "TTFB")
 puts "Test Passed" if loadtime && element
 puts loadtime.text, element.text
 
+
+emailbody = emailbody + "Values are #{loadtime.text}(Load Time), #{element.text}(First Byte), and is the website I used for #{urls}\n\n"
+
+end
+
+
 # Send Email
+
 config = YAML.load_file("cred.yml")
 
-gmail = Gmail.connect( config["config"]["email"], config["config"]["password"])
+gmail = Gmail.connect(config["config"]["email"], config["config"]["password"])
 
 puts gmail.logged_in?
 
 puts gmail.inbox.count
 
 email = gmail.compose do
-  to config["config"]["to"]
+  to config["config"]["email"]
   subject "I did it!"
-  body "Values are #{loadtime.text}(Load Time), #{element.text}(First Byte), and #{url} is the website I used for the web page test." 
+  body " #{emailbody} "
 end
 email.deliver!
 
